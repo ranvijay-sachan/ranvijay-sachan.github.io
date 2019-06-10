3521f74791230e3d9365efa71b5098c1d8a8c399
Ranvijay <sachan.ranvijay@altran.com>
2019-06-11 02:47:58 +0530

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-# My Portfolio
-Website containing details about me.
---
+++ precache-manifest.0cf05ecfbb287590276bb3ad943f68c9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee9108389cdcaebe0750e8dae7158f13",
+    "url": "/static/media/bg.ee910838.jpg"
+  },
+  {
+    "revision": "73bc78539fcdf0eb980c286ef702c1c1",
+    "url": "/static/media/RanvijaySachan.73bc7853.jpg"
+  },
+  {
+    "revision": "154d5f326bc974dadcbba627fe5792fa",
+    "url": "/static/media/Ranvijay Sachan - Resume.154d5f32.pdf"
+  },
+  {
+    "revision": "b6fbbf4e25b32aa991f5fd52a65babdc",
+    "url": "/static/media/Ranvijay Sachan - Coverletter.b6fbbf4e.pdf"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "53e0c8c5e32c867be3d9",
+    "url": "/static/js/main.53e0c8c5.chunk.js"
+  },
+  {
+    "revision": "82c96b85f52197d8d434",
+    "url": "/static/js/2.82c96b85.chunk.js"
+  },
+  {
+    "revision": "53e0c8c5e32c867be3d9",
+    "url": "/static/css/main.2629f91f.chunk.css"
+  },
+  {
+    "revision": "b7c870807d72ceafa769c2990f776eaf",
+    "url": "/index.html"
+  }
+];
